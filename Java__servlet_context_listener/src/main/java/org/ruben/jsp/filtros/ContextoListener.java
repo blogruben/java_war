fb57d01babd56be138b3d078f35be33d81d0e0bf
Ruben-R-C <Info@blogruben.com>
2023-07-13 22:54:43 +0200

    Fix servlet context listener

--- Java__servlet_context_listener/src/main/java/org/ruben/jsp/filtros/ContextoListener.java
+++ Java__servlet_context_listener/src/main/java/org/ruben/jsp/filtros/ContextoListener.java
@@ -8,13 +8,13 @@ public class ContextoListener implements ServletContextListener {
  
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        System.out.println("aplicacion web arrancada "+sce.getServletContext().getContextPath());
- 
+        System.out.println("Aplicacion Web Arrancada ");
+        System.out.println("El contento o la base de la URL es "+sce.getServletContext().getContextPath());
     }
  
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        System.out.println("aplicacion web parada");
+        System.out.println("Aplicacion Web Parada");
     }
  
 }
